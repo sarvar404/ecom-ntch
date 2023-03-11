62e9a470fed280e7ee5450ac105dbb4b97a4bd31
Sarvar <sarvar.dev404@gmail.com>
2023-03-12 02:41:25 +0530

    updated files second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Navbar,Footer} from './components';
+import Home from './Pages/Home';
+import ProductDetail from './Pages/ProductDetail';
+import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Router>
+        <Routes>
+          <Route
+          path='/'
+          element={<Home/>}>
+          </Route>
+
+          <Route
+          path='/product-detail'
+          element={<ProductDetail/>}>
+          </Route>
+
+        </Routes>
+      </Router>
+      <Footer/>
     </div>
   );
 }
